76f49446686b22287a29d8490ae63450bafc2482
RodrigoTeran <rodrigo.teranhernandez@gmail.com>
2022-07-02 11:25:18 -0700

    feat(): add link to project in landing page.

--- client/components/Landing/Landing.tsx
+++ client/components/Landing/Landing.tsx
@@ -3,7 +3,9 @@ import styles from './Landing.module.scss';
 const LandingPage = () => {
     return (
         <section className={styles.landing}>
-            <span>Teamplace</span> is coming...
+            <a rel="noreferrer" title="Open in Github" href="https://github.com/Rodrigo-s-Project" target="_blank">
+                <span>Teamplace</span> is coming...
+            </a>
         </section>
     );
 };
