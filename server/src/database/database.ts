d24994a61651867bd3b74c0ba00b2dd7fb7a2a99
RodrigoTeran <rodrigo.teranhernandez@gmail.com>
2022-07-05 19:31:33 -0700

    feat(db): add path on database connection

--- server/src/database/database.ts
+++ server/src/database/database.ts
@@ -1,11 +1,11 @@
 import { Sequelize } from "sequelize-typescript";
 
-export const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: "localhost",
-    dialect: "postgres"
+export const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
   }
-);
+});
