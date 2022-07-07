414c7bd93f20f0c951054d24b8234d441f5deec6
RodrigoTeran <rodrigo.teranhernandez@gmail.com>
2022-07-06 21:11:53 -0700

    styles(Dashboard): add menu to the aside on responsive mode

--- client/components/Dashboard/Menu/Menu.tsx
+++ client/components/Dashboard/Menu/Menu.tsx
@@ -1,9 +1,28 @@
 import styles from "./Menu.module.scss";
+import stylesNav from "../../Nav/DashboardNav/DashboardNav.module.scss";
+import { useContext } from "react";
+
+// Context
+import { GlobalContext } from "../../../pages/_app";
+
+// Components
+import { DashBoardNavControls } from "../../Nav/DashboardNav/DashboardNav";
 
 const MenuDashboard = () => {
+  const { isMenuOpen } = useContext(GlobalContext);
+
   return (
-    <aside className={styles.menu}>
-      <div>aside</div>
+    <aside className={`${styles.menu} ${isMenuOpen && styles.menu_open}`}>
+      <div className={styles.menu_nav}>
+        <div className={styles.menu_nav_title}>Menu</div>
+        <DashBoardNavControls
+          rules={{
+            ...stylesNav,
+            ...styles
+          }}
+        />
+      </div>
+      <div className={styles.menu_links}>Aside</div>
     </aside>
   );
 };
