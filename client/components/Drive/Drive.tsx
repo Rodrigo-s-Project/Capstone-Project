bd740f3fb717f2f67b411cd25aa1d6f8dce84d55
RodrigoTeran <rodrigo.teranhernandez@gmail.com>
2022-07-16 22:55:32 -0700

    styles(Dashboard): fix padding bottom not stretching when menu toggled on dashboard

--- client/components/Drive/Drive.tsx
+++ client/components/Drive/Drive.tsx
@@ -1,6 +1,5 @@
 import { useContext } from "react";
 import { GlobalContext } from "../../pages/_app";
-// import { DriveContext } from "./Provider";
 import styles from "./Drive.module.scss";
 
 // Components
@@ -8,12 +7,8 @@ import NavBarDrive from "./NavBar/NavBar";
 import AsideDrive from "./Aside/Aside";
 import BodyDrive from "./Body/Body";
 
-// TODO: issue between companies having same workspace
-
 const Drive = () => {
   const { selectedTeam } = useContext(GlobalContext);
-  // const {  } = useContext(DriveContext);
-
   return (
     <div className={styles.drive}>
       <h1>{selectedTeam && selectedTeam.name}&apos;s Workspace</h1>
