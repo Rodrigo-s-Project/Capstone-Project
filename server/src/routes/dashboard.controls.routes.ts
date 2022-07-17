4054daafeec90fa91520e7a589b5348934d2175b
RodrigoTeran <rodrigo.teranhernandez@gmail.com>
2022-07-17 12:51:12 -0700

    refactor(Client): refactor the aside lists in drive

--- server/src/routes/dashboard.controls.routes.ts
+++ server/src/routes/dashboard.controls.routes.ts
@@ -2,17 +2,9 @@ import express from "express";
 const router = express.Router();
 
 import { authenticate } from "../middlewares/auth/index";
-import { isAdmin } from "../middlewares/admin/index";
 import { editSection } from "../controllers/dashboard/controls/index";
 
 // Protected route
-router.put(
-  "/edit/:companyId",
-  authenticate,
-  (req, res, next) => {
-    isAdmin(req, res, next, "companyId");
-  },
-  editSection
-);
+router.put("/edit/:companyId", authenticate, editSection);
 
 export default router;
